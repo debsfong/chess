49d5bdb04b7f712174bd04e9f71311795691b788
App Academy Student <student@appacademy.io>
2016-09-19 15:18:21 -0700

    Created Board, Display, Piece, Cursor classes

---
+++ board.rb
@@ -0,0 +1,32 @@
+class Board
+  attr_reader :grid
+
+  def initialize
+    @grid = Array.new(8) { Array.new(8) { NullPiece.new } }
+  end
+
+  def move(start_pos, end_pos)
+    if @grid[start_pos].is_a?(NullPiece)
+      raise "There is no piece there"
+    elsif !valid_move?(start_pos, end_pos)
+      raise "That is not a valid move"
+    else
+      @grid[end_pos] = @grid[start_pos]
+      @grid[start_pos] = NullPiece.new
+    end
+  end
+
+
+
+  def [](pos)
+    row, col = pos
+    @grid[row][col]
+  end
+
+  def []=(pos, value)
+    row, col = pos
+    @grid[row][col] = value
+  end
+
+
+end
---
+++ piece.rb
@@ -0,0 +1,22 @@
+class Piece
+
+  def initialize(value = nil)
+    @value = value
+  end
+end
+
+class NullPiece
+
+  def initialize
+    @moves = nil
+    @color = color
+  end
+
+  def to_s
+
+  end
+
+  def empty?
+
+  end
+end
